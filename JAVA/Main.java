dea565fb9a7c5cc883ed4282a799ca3ea453fcb8
Shuv@25 <shuvsutradhar@gmail.com>
2024-10-12 01:56:50 +0530

    modified file

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -77,10 +77,10 @@ class NotAvailable extends Exception {
 }
 
 class holder implements Serializable {
-    Doubleroom luxury_doublerrom[] = new Doubleroom[10]; // Luxury
-    Doubleroom deluxe_doublerrom[] = new Doubleroom[20]; // Deluxe
-    Singleroom luxury_singleerrom[] = new Singleroom[10]; // Luxury
-    Singleroom deluxe_singleerrom[] = new Singleroom[20]; // Deluxe
+    Doubleroom luxury_doublerrom[] = new Doubleroom[10]; 
+    Doubleroom deluxe_doublerrom[] = new Doubleroom[20]; 
+    Singleroom luxury_singleerrom[] = new Singleroom[10]; 
+    Singleroom deluxe_singleerrom[] = new Singleroom[20]; 
 }
 
 class Hotel {
@@ -235,7 +235,7 @@ class Hotel {
         int j, count = 0;
         switch (i) {
             case 1:
-                for (j = 0; j < 10; j++) {
+                for (j = 0; j < hotel_ob.luxury_doublerrom.length; j++) {
                     if (hotel_ob.luxury_doublerrom[j] == null)
                         count++;
                 }
